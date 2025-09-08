fd4b5544384364d4278defd537f3016b3bab72da
FloresVillar <efloresv@uni.pe>
2025-09-08 12:55:29 -0500

    pytest

--- proyectos/Prueba_entrada_CC3S2/seccion2_python_git/tests/test_app.py
+++ proyectos/Prueba_entrada_CC3S2/seccion2_python_git/tests/test_app.py
@@ -1,7 +1,21 @@
 import pytest
-from app.app import summarize
+#from app.app import summarize
 #se define los datos reutilizables , luego los test unitarios propiamente
 #siguiendo el patron Arrange-Act-Assert (arrancar - actuar - afirmar) 
+
+def summarize(nums):  # TODO: tipado opcional 
+    if not nums:
+        raise ValueError("vacio")
+    numeros = []
+    for e in nums:
+        try:
+            f = float(e)
+            numeros.append(f)
+        except ValueError:
+            raise ValueError("no es numerico")
+    total = sum(numeros); cuenta = len(numeros) ; promedio = total/cuenta
+    return {"count":cuenta,"sum":total,"avg":promedio} 
+
 @pytest.fixture
 def datos_reutilizables(): #arrancamos
     return ["1", "2", "3"]
