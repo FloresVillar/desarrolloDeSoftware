57bbd4c6f85023efedab9b7fb4d52eb06bfa683c
FloresVillar <efloresv@uni.pe>
2025-09-24 08:41:31 -0500

    lab3

---
+++ labs/Laboratorio3/src/factories.py
@@ -0,0 +1,12 @@
+# pylint: disable=too-few-public-methods
+# src/factories.py
+import factory
+from src.carrito import Producto
+
+class ProductoFactory(factory.Factory):  # pylint: disable=too-few-public-methods
+    class Meta:
+        model = Producto
+
+    nombre = factory.Faker("word")
+    precio = factory.Faker("pyfloat", left_digits=2, right_digits=2, positive=True)
+#basaso en faker creamos ojetos produto con nombres y precios, hacemos legibles las pruebas
