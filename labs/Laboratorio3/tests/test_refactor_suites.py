7fab3248849b478a6511616ba61cd7770ab72fe8
FloresVillar <efloresv@uni.pe>
2025-10-09 11:47:55 -0500

    completa B3 refactor de suites

--- labs/Laboratorio3/tests/test_refactor_suites.py
+++ labs/Laboratorio3/tests/test_refactor_suites.py
@@ -3,7 +3,7 @@ from src.shopping_cart import *
 from unittest.mock import Mock
 
 class TestPrecisionMonetaria:
-    def test_suma_pequenias_cantidades():
+    def test_suma_pequenias_cantidades(self):
         shop = ShoppingCart()
         shop.add_item("x",1,0.05)
         shop.add_item("y",1,0.05)
@@ -11,4 +11,17 @@ class TestPrecisionMonetaria:
         assert round(total,2) == 0.1
 
 class TestPasarelaDePagos:
-    def test_pago_exitoso():
+    def test_pago_exitoso(self):
+        pgm = Mock()
+        pgm.process_payment.return_value = True
+        shop = ShoppingCart(payment_gateway=pgm)
+        shop.add_item("x",1,10.0)
+        resultado_pago = shop.process_payment(10.0)        
+        assert resultado_pago is True
+        pgm.process_payment.assert_called_once() 
+
+if __name__=='__main__':
+    test1 = TestPrecisionMonetaria()
+    test1.test_suma_pequenias_cantidades()
+    test2 = TestPasarelaDePagos()
+    test2.test_pago_exitoso()
