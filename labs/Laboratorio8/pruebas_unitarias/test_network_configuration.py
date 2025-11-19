fe61b5b87df0e39e6f448bc47ab83051e88a3b3f
FloresVillar <efloresv@uni.pe>
2025-11-19 09:53:08 -0500

    agrega el laboratorio8

--- labs/Laboratorio8/pruebas_unitarias/test_network_configuration.py
+++ labs/Laboratorio8/pruebas_unitarias/test_network_configuration.py
@@ -1,7 +1,7 @@
 #test_network_configuration.py
 import json
 import pytest
-CONFIG_FILE = 'network_config.json'
+CONFIG_FILE = 'network_config.json' #valida 
 
 @ pytest.fixture(scope="module")
 def conf():
@@ -11,4 +11,5 @@ def test_schema_keys(conf):
     assert isinstance(conf, dict)
     assert 'resources' in conf
     for res in conf['resources']:
-        assert 'type' in res and 'name' in res
+        assert 'type' in res and 'name' in res #valida type ,name;estatico
+#linter → apply
